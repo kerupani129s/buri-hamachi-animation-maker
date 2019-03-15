e6c531dd735ff95f932c49b528e8c1a9a597e3a5
ケルパニ＠猫 <32389989+kerupani129s@users.noreply.github.com>
2019-03-15 19:45:25 +0900

    add: アスペクト比を維持しながら画面サイズに合わせて canvas 表示 (Edge 未対応)

---
+++ docs/main.js
@@ -0,0 +1,15 @@
+(() => {
+
+	// 準備
+	const app = new PIXI.Application({width: 1600, height: 1200});
+
+	window.addEventListener('DOMContentLoaded', () => {
+		document.getElementById('canvas').appendChild(app.view);
+	});
+
+	const main = () => {
+	};
+
+	main();
+
+})();
